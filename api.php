a79d63e5a7b4f7a94d310361ef9a9f3f325825f1
Genius007L <2906932394@qq.com>
2021-01-28 00:10:00 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 $rand = rand(1,21);
-$url = 'https://cdn.jsdelivr.net/gh/Genius007L/photoapi/images/'.$rand.'.jpg';
+$url = 'https://cdn.jsdelivr.net/gh/Genius007L/photoapi@V-2.0/images'.$rand.'.jpg';
 $img = file_get_contents($url,true);
 header("Content-Type: image/jpeg;");
 echo $img;
