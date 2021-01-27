1225bbe8d4180c88fd1939fc87cc856b15fed423
Genius007L <2906932394@qq.com>
2021-01-27 23:39:28 +0800

    my photo
    my photo

---
+++ api.php
@@ -0,0 +1,8 @@
+<?php
+
+$rand = rand(1,21);
+$url = 'https://cdn.jsdelivr.net/gh/Genius007L/photoapi/images/'.$rand.'.jpg';
+$img = file_get_contents($url,true);
+header("Content-Type: image/jpeg;");
+echo $img;
+?>
