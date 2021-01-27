cde04d4a7161e0a0356a29194fd09c8bb9484021
Genius007L <2906932394@qq.com>
2021-01-28 00:03:33 +0800

    格式更换

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 $rand = rand(1,21);
-$url = 'https://cdn.jsdelivr.net/gh/Genius007L/photoapi/images/'.$rand.'.png';
+$url = 'https://cdn.jsdelivr.net/gh/Genius007L/photoapi/images/'.$rand.'.jpg';
 $img = file_get_contents($url,true);
 header("Content-Type: image/jpeg;");
 echo $img;
